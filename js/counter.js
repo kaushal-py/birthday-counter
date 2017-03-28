d9ec36d502487ee91498ec555eae2d88540cffd4
kb-studios <2015kaushal.bhogale@ves.ac.in>
2017-03-28 20:09:29 +0530

    reset timer

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 28, 2017 20:10:00").getTime();
+var countDownDate = new Date("Mar 29, 2017 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
